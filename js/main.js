32ef49bf2541977a0bad5cd8cb8d05dc2e449c6f
System Administrator <root@MacBook-Pro-Adam.local>
2020-06-05 12:09:39 +0200

    js v2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
-const $first = document.getElementById('first');
-const list =document.getElementsByClassName('list');
+// const $first = document.getElementById('first');
+// const list =document.getElementsById('list');
 
-function viewMenu() {
-    $first.addEventListener('click', viewList);
-}
-function viewList(){
-    list.classList.add('list-show');
 
-}
-document.addEventListener('DOMContentLoaded', main);
+// function viewMenu() {
+//     $first.addEventListener('click', viewList);
+// }
+// function viewList(){
+//     list.classList.add('list-show');
+    
+// }
+// function main() {
+//     viewMenu();
+    
+// }
+// document.addEventListener('DOMContentLoaded', main);
+
+window.onload = function() {
+    const first = document.getElementById('first');
+    const list = document.getElementById('list');
+    const close1 = document.getElementById('close1');
+    const close2 = document.getElementById('close2');
+    const close3 = document.getElementById('close3');
+
+    function viewMenu() {
+        first.addEventListener('click', viewList);
+    }
+    function closeMenu() {
+        close1.addEventListener("click", hideList);
+        close2.addEventListener("click", hideList);
+        close3.addEventListener("click", hideList);
+    }
+    function viewList(){
+        list.classList.toggle('list-show');
+    }
+    function hideList(){
+        list.classList.remove("list-show")
+    }
+    viewMenu();
+    closeMenu();
+  };
